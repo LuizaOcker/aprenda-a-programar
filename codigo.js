ee5ac3cf6b63f7672255fa5fd1c2587785501a49
Luiza Ocker <luiza.or@hotmail.com>
2022-08-23 20:45:44 -0300

    Meu primeiro código

---
+++ codigo.js
@@ -0,0 +1,6 @@
+let minhavariavel //Cria a variavel
+let outravariavel
+minhavariavel = "iTuring" //Define o valor a variavel
+outravariavel = "Aprenda A Programar"
+console.log(minhavariavel) //Exibe o valor da variavel
+console.log(outravariavel)
--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,3 @@
-console.log("Hello World!") 
-console.log("Hallo Welt!")  
+console.log("Hello World!")
+console.log("Hallo Welt!")
 console.log("Olá Mundo!")

408f32951b87df40fe929b5f854bf837b8e0458c
Luiza Ocker <luiza.or@hotmail.com>
2022-08-23 21:22:08 -0300

    Meu primeiro código

---
+++ calculo.js
@@ -0,0 +1,16 @@
+//let numero1 = 100 
+//let numero2 = 300
+
+//console.log(numero1 + numero2) //soma
+//console.log(numero1 - numero2) //subtrai
+//console.log(numero1 / numero2) //divide
+//console.log(numero1 * numero2) //multiplica
+
+//EXEMPLO   
+
+let contadobar = 1000
+let porcentagemgorgeta = 10
+let gorgetadogarcom = contadobar*porcentagemgorgeta/100
+
+console.log("A gorgeta do garçom é:")
+console.log(gorgetadogarcom + " reais")
--- codigo.js
+++ codigo.js
@@ -1,6 +1,13 @@
 let minhavariavel //Cria a variavel
 let outravariavel
+
 minhavariavel = "iTuring" //Define o valor a variavel
 outravariavel = "Aprenda A Programar"
+
 console.log(minhavariavel) //Exibe o valor da variavel
+console.log(outravariavel)
+
+minhavariavel = outravariavel
+
+console.log(minhavariavel)
 console.log(outravariavel)
